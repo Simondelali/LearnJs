9617c39189ac522150969c6808fd42417da5da7f
Simon Atiegar <atiegardelali@gmail.com>
2022-11-20 22:37:10 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-alert('JavaScript file linked Succesfully')
+/*alert('JavaScript file linked Succesfully')
 function sayHello(){
     console.log ('Hello guys')
 }
+*/
+
+function sayName(fn,ln){
+    console.log('Your full name is ' + fn + ln)
+
+}
+sayName('Simon','Atiegar')
