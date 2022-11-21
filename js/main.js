75fec6f14c7092b8eca66275b95d2aaa13ca4ef7
Simon Atiegar <atiegardelali@gmail.com>
2022-11-21 17:12:58 +0000

    update

--- js/main.js
+++ js/main.js
@@ -4,8 +4,28 @@ function sayHello(){
 }
 */
 
-function sayName(fn,ln){
-    console.log('Your full name is ' + fn + ln)
+function sayName(arg){
+    console.log(arg)
 
 }
 sayName('Simon','Atiegar')
+ function sum(num1,num2){
+    return num1 + num2
+ }
+ const result = sum(6,90)
+ console.log(result)
+sayName('Your name is simon Atiegar')
+
+const ten = () => 10
+const Ten = _ => 10
+console.log(ten(),Ten())
+
+const logger = (param) => {
+    console.log(param)
+}
+logger("The day is bright")
+
+const add = (n1 , n2) => {
+    return n1 + n2
+}
+console.log(add(2,67))
