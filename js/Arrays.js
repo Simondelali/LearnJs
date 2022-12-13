3283c683812c39c417d035dc3820e951af03a12f
Simon Atiegar <atiegardelali@gmail.com>
2022-12-13 23:57:44 +0000

    update

--- js/Arrays.js
+++ js/Arrays.js
@@ -1,1 +1,6 @@
 const emptyArray = ['cabbage','tomato sauce','salmon']
+//Array values
+/*Arrays can contain
+Primitives -> strings and numbers
+Objects -> Objects, other arrays and even functions*/
+ 
