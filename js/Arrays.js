25e3df4165c47485b37bc5008917e4ed8c3f1001
Simon Atiegar <atiegardelali@gmail.com>
2022-12-14 04:09:58 +0000

    update

--- js/Arrays.js
+++ js/Arrays.js
@@ -3,4 +3,28 @@ const emptyArray = ['cabbage','tomato sauce','salmon']
 /*Arrays can contain
 Primitives -> strings and numbers
 Objects -> Objects, other arrays and even functions*/
- 
+
+const strings = ['one','two','three']
+const numbers = [1,2,3,4]
+const booleans = [true,false]
+const objects = [{name:'Simon'},{name:'Dela'}]
+const arrays = [[1,2,3],[4,5,6]]
+
+//one item in one line
+const groceriesToBuy= [
+    'cabbage',
+    'tomato sauce',
+    'salmon'
+]
+
+//objects separated with new lines
+const object = [
+    {firstName: 'Simon', lastName: 'Dela'},
+    {firstName: 'Qhouphi', lastName: 'Cmon'}
+]
+
+//checking numer of items in an array
+console.log(numbers.length)
+
+//setting the vakue of an item
+strings[0] = 1
