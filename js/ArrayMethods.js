4e9a481f2c36600560c33fe8a4ce88cb80eae7d6
Simon Atiegar <atiegardelali@gmail.com>
2022-12-16 21:22:12 +0000

    update

---
+++ js/ArrayMethods.js
@@ -0,0 +1,50 @@
+/*
+1.Find the position of an item
+2.Add an item to the array
+3.Remove an item from the array
+*/
+
+// You can use indexOf 
+const fruitBasket = ['apple','banana','orange','pear']
+const posOfBanana = fruitBasket.indexOf('banana')
+const posOfKiwi = fruitBasket.indexOf('Banana')
+console.log(posOfKiwi,posOfBanana)
+
+/*
+indexOf only works with primitive values.
+If you want to find the index of an object, array,or function
+you need to use findIndex*/ 
+
+//Adding Items
+//Start
+const array = [3,4,5]
+array.unshift(2)
+array.unshift(4,8)
+//End
+array.push(78,90)
+console.log(array)
+
+//removing items
+//start
+const newArray = [3,4,5]
+const itemOne = newArray.shift()
+//End
+const lastItem = newArray.pop()
+console.log(newArray)
+
+//Splice Method
+//You can use splice to add items to the start of an array.
+
+const spliceArray =[3,4,5]
+spliceArray.splice(0,0,1,2)
+console.log(spliceArray)
+//Adding elements to the middle
+spliceArray.splice(3,0,6,7)
+console.log(spliceArray)
+
+//Deleting
+spliceArray.splice(0,3)
+console.log(spliceArray)
+
+
+
---
+++ js/exercise.js
@@ -0,0 +1,15 @@
+const myEmptyArray =[]
+const myFavTeams = [
+    'Argentina',
+    'Ghana',
+    'France'
+]
+
+let people = [
+    'Franklin Roosevelt',
+    'Thomas Edison',
+    'Benjamin Franklin'
+]
+people[1] = 'Inventor of the lightbulb'
+console.log(people[0])
+
