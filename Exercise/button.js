3869fc81ea7726564d1663c16d7b250127bf747c
Simon Atiegar <atiegardelali@gmail.com>
2022-12-24 23:37:03 +0000

    add css property selector

---
+++ Exercise/button.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('button')
+button.addEventListener('click',_=>{
+    button.style.color = 'cyan'
+    button.style.backgroundColor = 'blue'
+    button.style.width = '250px'
+    button.style.height = '125px'
+})
+
